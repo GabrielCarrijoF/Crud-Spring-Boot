9d1d1c028a37c754a17dc9aa02cacecff7e8496f
Gabriel Ferreira Carrijo <gabriel.carrijo@luizalabs.com>
2022-01-12 18:07:42 -0300

    projeto backend crud criado

---
+++ src/test/java/com/crud/crudlogin/CrudLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.crudlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
