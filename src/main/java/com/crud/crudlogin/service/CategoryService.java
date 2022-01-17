f9ff32b41401069a8e5a9dac48e16e8fd69e0263
Gabriel Ferreira Carrijo <gabriel.carrijo@luizalabs.com>
2022-01-17 12:29:33 -0300

    Segurança com Transactional

--- src/main/java/com/crud/crudlogin/service/CategoryService.java
+++ src/main/java/com/crud/crudlogin/service/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.crud.crudlogin.entitesCategorys.Category;
 import com.crud.crudlogin.repositories.CategoryRepository;
@@ -14,6 +15,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true) //
 	public List<Category> catchAll(){
 		return repository.findAll();
 	}
